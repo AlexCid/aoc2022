f4408d57285079d3ab9e1297572072c3f69ee116
Alexander Schaub <schaub.alexander@free.fr>
2022-12-09 10:17:18 +0100

    Resolved aoc8-2

--- d8-2/src/main.rs
+++ d8-2/src/main.rs
@@ -1,11 +1,10 @@
 use std::{
     fs::File,
     io::{BufRead, BufReader},
-    iter::once,
 };
 
 fn main() {
-    let trees = BufReader::new(File::open("input2.txt").unwrap())
+    let trees = BufReader::new(File::open("input.txt").unwrap())
         .lines()
         .map(|l| {
             l.unwrap()
@@ -20,35 +19,32 @@ fn main() {
     for i in 0..dim_x {
         for j in 0..dim_y {
             // to the right
-            let res = (1..i)
+            let up = (1..=i)
                 .map(|di| trees[i - di][j] < trees[i][j])
                 .enumerate()
                 .find(|v| !v.1)
                 .map(|(idx, _)| idx + 1)
-                .unwrap_or(i)
-                * (1..(dim_x - i))
-                    .map(|di| trees[i + di][j] < trees[i][j])
-                    .enumerate()
-                    .find(|v| !v.1)
-                    .map(|(idx, _)| idx + 1)
-                    .unwrap_or_else(|| dim_x.saturating_sub(i + 1))
-                * (1..(dim_y - j))
-                    .map(|dj| trees[i][j + dj] < trees[i][j])
-                    .enumerate()
-                    .find(|v| !v.1)
-                    .map(|(idx, _)| idx + 1)
-                    .unwrap_or_else(|| dim_y.saturating_sub(j + 1))
-                * (1..j)
-                    .rev()
-                    .map(|dj| trees[i][j - dj] < trees[i][j])
-                    .enumerate()
-                    .find(|v| !v.1)
-                    .map(|(idx, _)| idx + 1)
-                    .unwrap_or(j);
+                .unwrap_or(i);
+            let down = (1..(dim_x - i))
+                .map(|di| trees[i + di][j] < trees[i][j])
+                .enumerate()
+                .find(|v| !v.1)
+                .map(|(idx, _)| idx + 1)
+                .unwrap_or_else(|| dim_x.saturating_sub(i + 1));
+            let right = (1..(dim_y - j))
+                .map(|dj| trees[i][j + dj] < trees[i][j])
+                .enumerate()
+                .find(|v| !v.1)
+                .map(|(idx, _)| idx + 1)
+                .unwrap_or_else(|| dim_y.saturating_sub(j + 1));
+            let left = (1..=j)
+                .map(|dj| trees[i][j - dj] /*< trees[i][j]*/)
+                .enumerate()
+                .find(|v| v.1 >= trees[i][j])
+                .map(|(idx, _)| idx + 1)
+                .unwrap_or(j);
+            let res = left * right * down * up;
 
-            if res != 0 {
-                println!("{i}, {j}, {res}");
-            }
             if res > best_score {
                 best_score = res;
             }
